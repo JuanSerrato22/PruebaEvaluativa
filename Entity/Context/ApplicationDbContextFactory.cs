bb5bfd96fb089c35135865c1be1572baf9860c9e
Juan Serrato <serratojuan228@gmail.com>
2025-05-31 17:30:37 -0500

    cambios recientes

---
+++ Entity/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Entity.Context
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Asumimos que tu appsettings.json está en el proyecto Web
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Web");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(10, 4, 32)));
+
+            return new ApplicationDbContext(optionsBuilder.Options, configuration);
+
+        }
+    }
+}
